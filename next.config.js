06cf7dae19820b3717d6bc2967f831c2efbc74b9
AbdullahEsha <shahariaresha@gmail.com>
2025-07-09 18:11:05 +0600

    env update

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
       "www.flaticon.com",
     ], // <== Domain name
   },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
+      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+  },
 };
 
 module.exports = nextConfig;
